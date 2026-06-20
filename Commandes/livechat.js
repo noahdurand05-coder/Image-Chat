312fa2ae2eafec986a77ce8758926fcd2d0e6230
Noah <noah.durand@campus-eni.fr>
2026-06-20 19:03:55 +0200

    Last modif 20/06/2026

---
+++ Commandes/livechat.js
@@ -0,0 +1,11 @@
+const Discord = require("discord.js")
+
+module.exports = {
+
+    name: "livechat",
+
+    async execute(bot, message, args ){
+        message.reply("LiveChat envoyé !")
+    }
+
+}
---
+++ Commandes/ping.js
@@ -0,0 +1,11 @@
+const Discord = require("discord.js")
+
+module.exports = {
+
+    name: "ping",
+
+    execute(bot, message, args){
+        message.reply("Pong fils de pute de merde !")
+    }
+}
+        

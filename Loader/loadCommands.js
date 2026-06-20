531474584bf8f4394457b7cd17b7ed1909b6322c
Noah <noah.durand2026@campus-eni.fr>
2026-06-20 19:35:11 +0200

    Latest modif 20/06

--- Loader/loadCommands.js
+++ Loader/loadCommands.js
@@ -10,7 +10,8 @@ module.exports = async (bot) => {
         if(!command.name || typeof command.name !== "string") throw new TypeError(`La commande ${file.slice(0, file.length -3)} n'a pas de nom !`)
         
             bot.commands.set(command.name, command)
-            console.log(`Commande ${file} chargée avec succès !`)
+            console.log(`Commande ${file} chargée avec succès !`);
+            localStorage
     })
 
 }

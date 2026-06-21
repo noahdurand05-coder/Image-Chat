522c86e23827cfe5d617a40a2a6c8d4feb37f096
Suiki <suiki@users.noreply.github.com>
2026-06-21 11:56:44 +0200

    Création de la commande livchat 21/06/2026

--- Commandes/livechat.js
+++ Commandes/livechat.js
@@ -5,7 +5,37 @@ module.exports = {
     name: "livechat",
 
     async execute(bot, message, args ){
-        message.reply("LiveChat envoyé !")
+
+        const attachments = message.attachments;
+
+        
+
+        if(!attachments || attachments.size === 0){
+            return message.reply("❌ Tu dois envoyer une image ou une vidéo !")
+        }
+
+        const attachement = attachments.first();
+        const URL = attachement.url;
+        const texte = message.content.replace("!livechat", "").trim();
+        
+        if(!texte && URL){
+            message.reply(" ✅ LiveChat envoyé !")
+            console.log("Lien : ", URL);
+        }
+
+        if(texte && !URL){
+           return message.reply("❌ Tu dois envoyer une image ou une vidéo !")
+        }
+
+
+
+        if(texte && URL) {
+        console.log("Lien : ", URL);
+        console.log("Message : ", texte)
+        message.reply(" ✅ LiveChat envoyé !")
+        }
     }
 
 }
+
+
